7a966917a78eb6b1c580c0de9ba98c6be4c42ba2
kazuki.nara <kazuki.nara@gmail.com>
2016-06-26 11:07:22 +0900

    :heavy_plus_sign: Add sample of LevelListAnimatedVectorDrawable.

--- app/src/main/java/com/github/kazukinr/android/samples/MainActivity.java
+++ app/src/main/java/com/github/kazukinr/android/samples/MainActivity.java
@@ -1,7 +1,13 @@
 package com.github.kazukinr.android.samples;
 
+import com.github.kazukinr.android.samples.level_list_animated_vector.Activity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.levelListAnimatedVector)
+    void toLevelListAnimatedVector() {
+        startActivity(new Intent(this, Activity.class));
     }
 }
